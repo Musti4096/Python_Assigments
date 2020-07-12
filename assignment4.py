b07bfc7931d512bcfcc0a888f17ebbae8475d7ed
Musti4096 <53371560+Musti4096@users.noreply.github.com>
2020-07-12 18:54:22 +0200

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -1,6 +1,7 @@
 number = int(input("Please enter a number"))
 
-if (number % 1 == 0)  and (number % 2 != 0) and (number % 5 != 0):
+
+if (number % 1 == 0) and (number % number == 0 ) and (number % 2 != 0) and (number % 3 != 0) and (number % 5 != 0) and (number % 7 != 0):
     print(f"{number} is a prime number")
 else:
     print(f"{number} is not a prime number")
